983ec193c1c3f526ba5d18907266f1af75f9da84
imarkwick <izzymarkwick@gmail.com>
2014-12-29 22:03:42 +0000

    sockets working

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var socket = require('./src/socketController.js')(io);
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -31,8 +32,8 @@ app.get('/about', function(req, res) {
 });
 
 // instant chat page
-app.get('/chat', function(req, res) {
-	res.render('sockets');
+app.get('/sockets', function(request, response) {
+	response.render('pages/sockets');
 });
 
 server.listen(3000, function() {
