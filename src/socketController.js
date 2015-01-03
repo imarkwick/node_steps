5a2a0d1eb8508cc3cb099f991927e04c93914f45
imarkwick <izzymarkwick@gmail.com>
2015-01-03 15:38:23 +0000

    messages posting

--- src/socketController.js
+++ src/socketController.js
@@ -2,6 +2,14 @@ var socket = function(io) {
 
 	io.on('connection', function(socket) {
 		console.log('Client connected');
+
+		socket.on('disconnect', function(){
+			console.log('Client disconnected');
+		});
+
+		socket.on('chat message', function(msg) {
+			io.emit('chat message', msg);
+		});
 	});
 };
 
